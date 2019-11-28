1b377446a698872174b705f51eabf41233d4b399
nobsessive <33778469+nobsessive@users.noreply.github.com>
2019-11-28 04:59:44 -0500

    Stepped out the first step to call different solver by using the solver's name

---
+++ leetcode/vProj/vProj/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  vProj
+//  Copyright © 2019 nobsessive. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ leetcode/vProj/vProj/vProj.h
@@ -0,0 +1,17 @@
+//
+//  vProj.h
+//  vProj
+//
+//  Created by 小小灰 on 2019/11/28.
+//  Copyright © 2019 nobsessive. All rights reserved.
+//
+
+#ifndef vProj_h
+#define vProj_h
+
+class Solver
+{
+    Solver()
+};
+
+#endif /* vProj_h */
