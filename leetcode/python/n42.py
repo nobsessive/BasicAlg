58f4552fc0f38764d755de174f4eb3f284db8715
Pengzhou He <livingpz@gmail.com>
2021-08-31 16:32:33 -0400

    added n42.py, under developing

---
+++ leetcode/python/n42.py
@@ -0,0 +1,8 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+if __name__=="__main__":
+    s=Solution()
+    sample=[0,1,0,2,1,0,1,3,2,1,2,1]
+    for i in sample:
+        print(s.lengthOfLongestSubstring(i))
